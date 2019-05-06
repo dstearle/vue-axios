f70ef2d80246d4f46ce373217c95575a3bae18f8
mt-apato <dallas.st.earle@gmail.com>
2019-05-05 23:58:56 -0700

    Added interceptors to the main.js

--- src/main.js
+++ src/main.js
@@ -12,6 +12,20 @@ axios.defaults.baseURL = 'https://vue-axios-cc3ce.firebaseio.com'
 axios.defaults.headers.common['Authorization'] = 'something here'
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
+// Some examples of axios interceptors
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request Interceptor', config)
+  return config
+})
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Response Interceptor', res)
+  return res
+})
+
+// Some examples of interceptor ejectors. Will prevent the interceptors from occuring when uncommented
+// axios.interceptors.request.eject(reqInterceptor)
+// axios.interceptors.response.eject(resInterceptor)
+
 new Vue({
   el: '#app',
   router,
